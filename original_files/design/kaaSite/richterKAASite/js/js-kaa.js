724840dd63e2f761a988334fea0ddc3f6e4b8d94
atrichter <andrewrichter22@gmail.com>
2020-06-13 16:42:52 -0500

    add design files 2

---
+++ original_files/design/kaaSite/richterKAASite/js/js-kaa.js
@@ -0,0 +1,8 @@
+function menu() {
+  var x = document.getElementById("navAltLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
