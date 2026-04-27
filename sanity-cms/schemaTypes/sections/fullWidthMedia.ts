0d823c8f0baf2c7bf2444e44c0be5c270c9652ad
Andrew Richter <andrewrichter22@gmail.com>
2026-04-27 13:29:56 -0700

    Update fullWidthMedia section

--- sanity-cms/schemaTypes/sections/fullWidthMedia.ts
+++ sanity-cms/schemaTypes/sections/fullWidthMedia.ts
@@ -1,5 +1,6 @@
 import { defineType, defineField } from 'sanity'
 import { ImageIcon } from '@sanity/icons'
+import { styledTextFieldValidation } from '../types/styledText'
 
 export const sectionFullWidthMediaName = 'section.fullWidthMedia'
 
@@ -10,22 +11,32 @@ export default defineType({
   type: 'document',
   fields: [
     defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'styledHeadingText',
+      validation: styledTextFieldValidation(true),
+    }),
+    defineField({
+      name: 'subtitle',
+      title: 'Subtitle',
+      type: 'styledHeadingText',
+    }),
+    defineField({
       name: 'image',
       title: 'Image',
       type: 'image',
       options: { hotspot: true },
       validation: (Rule) => Rule.required(),
     }),
-    defineField({
-      name: 'altText',
-      title: 'Alternative Text',
-      type: 'string',
-      validation: (Rule) => Rule.required(),
-    }),
   ],
   preview: {
-    prepare() {
-      return { title: 'Full Width Media' }
+    select: { title: 'title.text', subtitle: 'subtitle.text', media: 'image' },
+    prepare({ title, subtitle, media }) {
+      return {
+        title: title || 'Card',
+        subtitle: subtitle || undefined,
+        media,
+      }
     },
   },
 })
