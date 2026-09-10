3cda12059636bbeff7bd4e2d8528f67a1387e45a
Andrew Richter <andrewrichter22@gmail.com>
2026-09-10 00:03:36 -0700

    Update font imports

--- svelte-app/src/routes/+layout.svelte
+++ svelte-app/src/routes/+layout.svelte
@@ -8,9 +8,12 @@
 <svelte:head>
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-  <link href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&display=swap" rel="stylesheet" />
   <link
-    href="https://fonts.googleapis.com/css2?family=STIX+Two+Text:wght@400;500;600;700;800&display=swap"
+    href="https://fonts.googleapis.com/css2?family=STIX+Two+Text:ital,wght@0,400..700;1,400..700&display=swap"
+    rel="stylesheet"
+  />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=STIX+Two+Text:ital,wght@0,400..700;1,400..700&display=swap"
     rel="stylesheet"
   />
   <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
